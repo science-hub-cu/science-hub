7b51b22540a343dd7004c8d276d9bae3af6a355e
omar <202027471@std.sci.cu.edu.eg>
2023-04-07 01:12:49 +0200

    enable ertry page

--- App.js
+++ App.js
@@ -1,33 +1,55 @@
-import React, { useState, useEffect } from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import RegistrationScreen from "./screens/auth/Register";
-import HomeScreen from "./screens/Home";
-import { StyleSheet, Text, View } from "react-native";
-import COLORS from "./assets/colors";
-//-------------------------------------------------------------------------------//
-const Stack = createStackNavigator();
+import React, { useState, useEffect } from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+import { StatusBar } from 'expo-status-bar'
+import RegistrationScreen from './screens/auth/Register'
+import HomeScreen from './screens/Home'
+import { StyleSheet, Text, View } from 'react-native'
+import COLORS from './assets/colors'
+import RetryScreen from './screens/retry'
+import NetInfo from '@react-native-community/netinfo'
+
+const Stack = createStackNavigator()
 
 const App = () => {
+  const [isNetConnected, setIsNetConnected] = useState(false)
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener(state => {
+      console.log('Connection type', state.type)
+      console.log('Is connected?', state.isConnected)
+      setIsNetConnected(state.isConnected)
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  }, []) // add empty array to run only once
+
+  if (!isNetConnected) {
+    return <RetryScreen />
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen
-          name="RegistrationScreen"
+          name='RegistrationScreen'
           component={RegistrationScreen}
         />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name='HomeScreen' component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
-};
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.mainBackground,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-export default App;
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+export default App
