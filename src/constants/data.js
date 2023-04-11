13a58f706169ffc314c88e15012be946c84d1bdb
Mahmoud Atef <mahmoudatef.coder@gmail.com>
2023-04-11 10:52:27 +0200

    add data constant

---
+++ src/constants/data.js
@@ -0,0 +1,16 @@
+export const APP_DOMAIN_FOR_EMAILS = "hamoksha.sci.app.com";
+export const Departments = [
+  { label: "Computer Scince", key: "1" },
+  { label: "Chem", key: "2" },
+  { label: "Ph", key: "3" },
+  { label: "math", key: "4" },
+  { label: "math/CS", key: "5" },
+  { label: "Bio", key: "6" },
+  { label: "keko", key: "7" },
+];
+export const levels = [
+  { label: "level 1", value: "1" },
+  { label: "level 2", value: "2" },
+  { label: "level 3", value: "3" },
+  { label: "level 4", value: "4" },
+];
