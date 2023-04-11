92695165b08167dd7045d18c33c33e3b1de8f366
hngara <midomada45@gmail.com>
2023-04-11 03:51:32 +0200

    aya 7aga ll task

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-const HomeScreen = ({ navigation }) => {
-  return <SafeAreaView></SafeAreaView>;
+import { signOut } from "firebase/auth";
+import { auth } from "../services/auth/firebase";
+const HomeScreen = ({ route, navigation }) => {
+  const handlesignout = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
+  return (
+    <SafeAreaView>
+      <Text>hello,{route.params}</Text>
+    </SafeAreaView>
+  );
 };
 export default HomeScreen;
