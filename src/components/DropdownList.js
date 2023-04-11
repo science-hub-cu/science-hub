b54bf8d9c18e6603c3f92aba7de90f2195670979
omar <202027471@std.sci.cu.edu.eg>
2023-04-11 03:14:40 +0200

    modify dropplist (enable set errors)

--- src/components/DropdownList.js
+++ src/components/DropdownList.js
@@ -1,20 +1,25 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import COLORS from '../constants/colors'
-import { Dropdown } from 'react-native-element-dropdown'
+import React, { forwardRef } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import COLORS from "../constants/colors";
+import { Dropdown } from "react-native-element-dropdown";
 
-const DropdownList = ({
-  labelField,
-  valueField,
-  placeholder,
-  data,
-  value,
-  onChange
-}) => {
+const DropdownList = (
+  { labelField, valueField, placeholder, data, value, onChange, error },
+  ref
+) => {
   return (
     <View style={styles.view}>
       <Dropdown
-        style={styles.dropdown}
+        ref={ref}
+        style={[
+          styles.dropdown,
+          {
+            borderWidth: 1,
+            borderColor: error
+              ? COLORS.red
+              : COLORS.white,
+          },
+        ]}
         placeholderStyle={styles.placeholderStyle}
         selectedTextStyle={styles.selectedTextStyle}
         data={data}
@@ -25,49 +30,58 @@ const DropdownList = ({
         maxHeight={300}
         search={0}
         onChange={onChange}
-        renderItem={item => (
+        renderItem={(item) => (
           <View style={styles.item}>
             <Text style={styles.textItem}>{item[labelField]}</Text>
           </View>
         )}
       />
+      {error && <Text style={styles.errors}>{error}</Text>}
     </View>
-  )
-}
+  );
+};
 const styles = StyleSheet.create({
   view: {
-    alignItems: 'center',
+    // alignItems: "center",
     marginTop: 25,
-    flex: 1
+    // flex: 1,
   },
   dropdown: {
     borderWidth: 1,
-    borderColor: COLORS.white,
+    //  borderColor: COLORS.white,
     height: 55,
-    width: '88%',
+    width: "88%",
     backgroundColor: COLORS.secBackground,
     borderRadius: 9,
-    padding: 12
+    padding: 12,
+    // alignItems: "center",
+    marginHorizontal: "6%",
   },
   item: {
     padding: 18,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: COLORS.secBackground
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: COLORS.secBackground,
   },
   textItem: {
     flex: 1,
     fontSize: 16,
-    color: '#fff'
+    color: COLORS.white,
   },
   placeholderStyle: {
     fontSize: 16,
-    color: COLORS.gray2
+    color: COLORS.gray2,
   },
   selectedTextStyle: {
     fontSize: 16,
-    color: '#fff'
-  }
-})
-export default DropdownList
+    color: COLORS.white,
+  },
+  errors: {
+    color: COLORS.red,
+    marginHorizontal: "8%",
+  },
+});
+const forwardDropdownList = forwardRef(DropdownList);
+
+export default forwardDropdownList;
