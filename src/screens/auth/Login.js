abe26a7cf358f52277c78369d430edaf8604eb6a
omar <202027471@std.sci.cu.edu.eg>
2023-04-13 01:18:23 +0200

    delete handle error and validition from login

--- src/screens/auth/Login.js
+++ src/screens/auth/Login.js
@@ -9,11 +9,6 @@ const LoginScreen = ({ navigation }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
-
-  // change to error state if there is error in any field
-  const handleError = (errorMessage, input) => {
-    setErrors((prevState) => ({ ...prevState, [input]: errorMessage }));
-  };
   const LoginPress = () => {
     let user = {
       username,
@@ -27,47 +22,6 @@ const LoginScreen = ({ navigation }) => {
       console.log("no");
     }
   };
-  const validate = () => {
-    Keyboard.dismiss();
-    let valid = true;
-    return valid;
-    if (inputs.name === "") {
-      handleError("please enter your name", "name");
-      valid = false;
-    } else if (!inputs.name.match(/^[A-Za-z]+$/)) {
-      handleError("your name should contains only letters", "name");
-      valid = false;
-    }
-    if (isRegister) {
-      if (!inputs.code) {
-        handleError("please enter your code", "code");
-        valid = false;
-      } else if (!inputs.code.match(/^[0-9]+$/)) {
-        handleError("please enter valid code", "code");
-        valid = false;
-      } else if (inputs.code.length != 7) {
-        handleError("the code must be 7 numbers", "code");
-        valid = false;
-      }
-      if (!inputs.level) {
-        handleError("please choose your level", "level");
-        valid = false;
-      }
-      if (!inputs.department) {
-        handleError("please choose your department", "department");
-        valid = false;
-      }
-    }
-    if (!inputs.password) {
-      handleError("please enter your password", "password");
-      valid = false;
-    } else if (inputs.password.length < 6) {
-      handleError("password is to small", "password");
-      valid = false;
-    }
-    return valid;
-  };
-
   return (
     <View>
       <View style={styles.centerAligment}>
