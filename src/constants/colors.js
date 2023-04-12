e419bf96c28dfa74a54adeef6f82aed8d2a1f8c6
aboelyazeed <abdelrahman.m.aboelyazeed@gmail.com>
2023-04-12 14:51:40 +0200

    add greenCOLOR

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,22 +1,20 @@
 const COLORS = {
-    black:'#000',
-    blue:'#5D5FEE',
-    light:'#F3F4FB',
-    darkBlue:'#7978B5',
-    red:'red',
-    white :'#fff',
-    gray:'#4B4D52',
-    button:'#5865F2',
-    mainBackground:'#2B2D31',
-    secBackground:'#1E1F22',
-    navBar:'#201D1D',
-    navBarBackground:'#27282A',
-    gray2:'#808080',
-    starActive:'#EFDE44',
-    upvote:'#1120C5',
-    navbar:'#201D1D'
-
-
-
+  black: "#000",
+  blue: "#5D5FEE",
+  light: "#F3F4FB",
+  darkBlue: "#7978B5",
+  red: "red",
+  white: "#fff",
+  gray: "#4B4D52",
+  button: "#5865F2",
+  mainBackground: "#2B2D31",
+  secBackground: "#1E1F22",
+  navBar: "#201D1D",
+  navBarBackground: "#27282A",
+  gray2: "#808080",
+  starActive: "#EFDE44",
+  upvote: "#1120C5",
+  navbar: "#201D1D",
+  green: "#20771E",
 };
 export default COLORS;
