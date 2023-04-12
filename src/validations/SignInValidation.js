9419e8c1395f1538350918b18778ce8b39a5b538
omar <202027471@std.sci.cu.edu.eg>
2023-04-13 01:22:44 +0200

    add sign in validation in separated file

--- src/validations/SignInValidation.js
+++ src/validations/SignInValidation.js
@@ -1,1 +1,36 @@
-export default SignInValidation = {};
+export const signInValidation = (
+  { username ,password},
+  setErrors
+) => {
+  let valid = true;
+  if (username === "") {
+    setErrors((prevState) => ({
+      ...prevState,
+      username: "please enter your name",
+    }));
+    valid = false;
+  } else if (!username.match(/^[A-Za-z]+$/)) {
+    setErrors((prevState) => ({
+      ...prevState,
+      username: "your name should contains only letters",
+    }));
+    valid = false;
+  }
+  if (!password) {
+    setErrors((prevState) => ({
+      ...prevState,
+      password: "please enter your password",
+    }));
+    valid = false;
+  } else if (password.length < 6) {
+    setErrors((prevState) => ({
+      ...prevState,
+      password: "password is too small",
+    }));
+    valid = false;
+  }
+  return valid;
+};
+export const disappearError = (inputName, errors, setErrors) => {
+  setErrors((prevState) => ({ ...prevState, [inputName]: "" }));
+};
