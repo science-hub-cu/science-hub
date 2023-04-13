3ebf6b848b4a431dab54b54116c68d09c1684d8c
aboelyazeed <abdelrahman.m.aboelyazeed@gmail.com>
2023-04-13 23:34:05 +0200

    scroll between screens

--- src/screens/auth/authScreen.js
+++ src/screens/auth/authScreen.js
@@ -1,69 +1,116 @@
-import React, { useState } from "react";
-import { Text, StyleSheet, View} from "react-native";
+import React, { useState, useRef } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  Text,
+  StyleSheet,
+  View,
+  Animated,
+  Dimensions,
+  PanResponder,
+} from "react-native";
+import { PanGestureHandler } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView, Directions } from "react-native-gesture-handler";
 import COLORS from "../../constants/colors";
-import { ScrollView } from "react-native-gesture-handler";
 import RegistrationScreen from "./Register";
 import LoginScreen from "./Login";
+
+const screenWidth = Dimensions.get("window").width;
+
 const AuthScreen = ({ navigation }) => {
-  const [isRegister, setIsRegister] = useState(true);
+  const [isRegister, setIsRegister] = useState(false);
+
+  const { width, height } = Dimensions.get("window");
+
+  const [sliderLocation, setSliderLocation] = useState(0);
+
+  const setSliderPage = (event) => {
+    const { x } = event.nativeEvent.contentOffset;
+    const max = (width * 40) / 100;
+    let v = (x * 40) / 100;
+
+    setIsRegister(v >= max / 2 ? 1 : 0);
+
+    setSliderLocation(Math.min(max, Math.round(v)));
+    console.log(v);
+    console.log(max);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        <Text style={styles.headerText}>
-          {isRegister ? "Register" : "Login"}
-        </Text>
-        <Text
-          style={{
-            fontSize: 25,
-            textAlign: "center",
-            color: COLORS.white,
-            fontWeight: 400,
-            fontFamily: "majalla",
-            paddingTop: 15,
-          }}
-        >
-          {"  "}By signing in you are agreeing
-        </Text>
-        <Text style={styles.text}>
-          {"  "}our
-          <Text style={{ color: COLORS.blue }}> Term and privacy policy</Text>
-        </Text>
-        <View style={styles.rowView}>
-          <Text
-            style={{
-              textAlign: "center",
-              color: isRegister ? COLORS.white : COLORS.blue,
-              fontFamily: "majalla",
-              fontSize: 25,
-              borderBottomWidth: !isRegister ? 1 : 0,
-              borderBottomColor: COLORS.white,
-            }}
-            onPress={() => {
-              setIsRegister(false);
-            }}
+        <StatusBar barStyle="dark-content" />
+        <View>
+          <View>
+            <Text style={styles.headerText}>
+              {isRegister ? "Register" : "Login"}
+            </Text>
+            <Text
+              style={{
+                fontSize: 25,
+                textAlign: "center",
+                color: COLORS.white,
+                fontWeight: 400,
+                fontFamily: "majalla",
+                paddingTop: 15,
+              }}
+            >
+              {"  "}By signing in you are agreeing
+            </Text>
+            <Text style={styles.text}>
+              {"  "}our
+              <Text style={{ color: COLORS.blue }}>
+                {" "}
+                Term and privacy policy
+              </Text>
+            </Text>
+          </View>
+          <View style={styles.rowView}>
+            <Text
+              style={{
+                textAlign: "center",
+                color: isRegister ? COLORS.white : COLORS.blue,
+                fontFamily: "majalla",
+                fontSize: 25,
+              }}
+            >
+              Login
+            </Text>
+            <Text
+              style={{
+                textAlign: "center",
+                color: isRegister ? COLORS.blue : COLORS.white,
+                fontFamily: "majalla",
+                fontSize: 25,
+                marginLeft: "10%",
+              }}
+            >
+              Register
+            </Text>
+          </View>
+          <View style={{ alignItems: "center" }}>
+            <View
+              style={[styles.paginationDots, { marginLeft: sliderLocation }]}
+            />
+          </View>
+
+          <ScrollView
+            style={{ flex: 1 }}
+            horizontal={true}
+            scrollEventThrottle={16}
+            pagingEnabled={true}
+            showsHorizontalScrollIndicator={false}
+            bounces={false}
+            onScroll={setSliderPage}
           >
-            Login
-          </Text>
-          <Text
-            style={{
-              textAlign: "center",
-              color: isRegister ? COLORS.blue : COLORS.white,
-              fontFamily: "majalla",
-              fontSize: 25,
-              marginLeft: "10%",
-              borderBottomWidth: isRegister ? 1 : 0,
-              borderBottomColor: COLORS.white,
-            }}
-            onPress={() => {
-              setIsRegister(true);
-            }}
-          >
-            Register
-          </Text>
+            <View style={{ width: screenWidth }}>
+              <LoginScreen />
+            </View>
+            <View style={{ width: screenWidth }}>
+              <RegistrationScreen />
+            </View>
+          </ScrollView>
         </View>
-        {isRegister ? <RegistrationScreen /> : <LoginScreen />}
       </ScrollView>
     </SafeAreaView>
   );
@@ -92,9 +139,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   rowView: {
-    paddingTop: "10%",
+    paddingTop: 15,
     marginLeft: "32.25%",
     flexDirection: "row",
+  },
+  paginationDots: {
+    marginRight: "25%",
+    height: 2,
+    width: "18%",
+    backgroundColor: "#fff",
   },
 });
 export default AuthScreen;
