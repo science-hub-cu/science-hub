026ed45f5f3bc077dc99aed1df28fbadcdb7d50c
omar <202027471@std.sci.cu.edu.eg>
2023-04-14 16:02:01 +0200

    separate all validations methods

--- src/validations/CommonValidation.js
+++ src/validations/CommonValidation.js
@@ -1,3 +1,15 @@
 export const isValidFacultyCode = (code) => {
   return code !== undefined && code.match(/^[0-9]+$/) && code.length === 7;
 };
+export const isValidUserName = (username) => {
+  return username !== "" && username.match(/^[A-Za-z]+$/);
+};
+export const isValidLevel = (level) => {
+  return level.label;
+};
+export const isValidDepartment = (department) => {
+  return department.label;
+};
+export const isValidPassword = (password) => {
+  return password !== undefined && password.length > 6;
+};
