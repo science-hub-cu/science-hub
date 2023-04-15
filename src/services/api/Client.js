74a763969c45c4d0bd793ddb23a5d3044d07556f
Mahmoud Atef <mahmoudatef.coder@gmail.com>
2023-04-15 08:36:19 +0200

    update api url

--- src/services/api/Client.js
+++ src/services/api/Client.js
@@ -6,7 +6,7 @@
 import axios from "axios";
 
 export default class Client {
-  static apiUrl = "http://192.168.1.3:5000/api";
+  static apiUrl = "https://cs303-backend-server.onrender.com/api";
   static endPointToUrl(endPoint) {
     return `${Client.apiUrl}${endPoint}`;
   }
