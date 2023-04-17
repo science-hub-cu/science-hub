56789148d9fbe30a7a04dc5fee57ade4afba10ca
omar <202027471@std.sci.cu.edu.eg>
2023-04-17 06:42:39 +0200

    add header font and style it

--- src/layouts/DrawerNavigator.js
+++ src/layouts/DrawerNavigator.js
@@ -2,12 +2,13 @@ import React from "react";
 import { View, SafeAreaView, StyleSheet, Text } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import Animated from "react-native-reanimated";
+import { useFonts } from "expo-font";
+
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
 } from "@react-navigation/drawer";
 import COLORS from "../constants/colors";
-
 import HomeScreen from "../screens/Home";
 const Drawer = createDrawerNavigator();
 const MyDrawer = () => {
@@ -18,8 +19,10 @@ const MyDrawer = () => {
         drawerStyle: {
           width: "85%",
           backgroundColor: COLORS.navBarBackground,
+          borderRightWidth: 10,
+          borderRightColor: COLORS.mainBackground,
         },
-        drawerType:"slide"
+        drawerType: "slide",
       }}
     >
       <Drawer.Screen name="HomeScreen" component={HomeScreen} />
@@ -27,18 +30,25 @@ const MyDrawer = () => {
   );
 };
 const CustomDrawerContent = (props) => {
+      const [fontLoaded] = useFonts({
+        Trebuchet: require("../assets/fonts/trebuc.ttf"),
+      });
+      if (!fontLoaded) return null;
   return (
-    <DrawerContentScrollView
-      {...props}
-      
-    >
-      <View style={{ flexDirection: "row" }}>
+    <DrawerContentScrollView {...props}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <Ionicons
           name="notifications"
           size={32}
           style={{ color: COLORS.white, marginTop: "2%" }}
         />
-        <Text style={styles.title}>Notifications</Text>
+        <Text style={styles.title}> Notifications</Text>
       </View>
     </DrawerContentScrollView>
   );
@@ -48,9 +58,7 @@ const styles = StyleSheet.create({
     color: COLORS.white,
     fontWeight: 400,
     fontSize: 32,
-    textAlign: "center",
-    flex: 1,
-    textAlignVertical: "center",
+    fontFamily: "Trebuchet",
   },
 });
 export default MyDrawer;
