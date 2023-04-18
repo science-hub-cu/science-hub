244233fc4943cf64f78ccc76534d9c4ebc779bc1
Mahmoud Atef <mahmoudatef.coder@gmail.com>
2023-04-18 06:56:55 +0200

    update loading button

--- src/components/loadingButton.js
+++ src/components/loadingButton.js
@@ -1,65 +1,75 @@
-import React, { useState } from "react";
+import React, { useImperativeHandle, useState } from "react";
 import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
 import COLORS from "../constants/colors";
+import { forwardRef } from "react";
 
-const LoadingButton = ({
-  title,
-  onPress = () => {},
-  loading,
-  width = "88%",
-}) => {
-  const [isLoading, setIsLoading] = useState(false);
+const LoadingButton = forwardRef(
+  ({ title, onPress = () => {}, width = "88%" }, ref) => {
+    const [isLoading, setIsLoading] = useState(false);
 
-  const handlePress = () => {
-    setIsLoading(true);
-    onPress();
-  };
+    const handlePress = () => {
+      setIsLoading(true);
+      onPress();
+    };
 
-  return (
-    <TouchableOpacity
-      activeOpacity={0.7}
-      onPress={handlePress}
-      disabled={isLoading || loading}
-      style={{
-        marginTop: 20,
-        height: 50,
-        width: width,
-        backgroundColor: isLoading || loading ? COLORS.load : COLORS.blue,
-        alignItems: "center",
-        justifyContent: "center",
-        borderRadius: 25,
-        marginHorizontal: 10,
-      }}
-    >
-      {isLoading || loading ? (
-        <View style={{ flexDirection: "row" }}>
+    useImperativeHandle(
+      ref,
+      () => {
+        return {
+          setLoading(load) {
+            setIsLoading(load);
+          },
+        };
+      },
+      []
+    );
+
+    return (
+      <TouchableOpacity
+        activeOpacity={0.7}
+        onPress={handlePress}
+        disabled={isLoading}
+        style={{
+          marginTop: 0,
+          height: 50,
+          width: width,
+          backgroundColor: isLoading ? COLORS.load : COLORS.blue,
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 25,
+          marginHorizontal: 10,
+        }}
+      >
+        {isLoading ? (
+          <View style={{ flexDirection: "row" }}>
+            <Text
+              style={{
+                color: COLORS.white,
+                fontWeight: "bold",
+                fontSize: 16,
+                textAlign: "center",
+                marginRight: 10,
+              }}
+            >
+              {title}
+            </Text>
+            <ActivityIndicator color="#fff" />
+          </View>
+        ) : (
           <Text
             style={{
               color: COLORS.white,
               fontWeight: "bold",
               fontSize: 16,
               textAlign: "center",
-              marginRight: 10,
             }}
           >
             {title}
           </Text>
-          <ActivityIndicator color="#fff" />
-        </View>
-      ) : (
-        <Text
-          style={{
-            color: COLORS.white,
-            fontWeight: "bold",
-            fontSize: 16,
-            textAlign: "center",
-          }}
-        >
-          {title}
-        </Text>
-      )}
-    </TouchableOpacity>
-  );
-};
+        )}
+      </TouchableOpacity>
+    );
+  }
+);
 
 export default LoadingButton;
