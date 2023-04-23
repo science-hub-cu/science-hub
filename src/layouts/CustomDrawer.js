7c8e7a4f2f8c17d61a72c2ca2a5b490c57c955e6
omar <202027471@std.sci.cu.edu.eg>
2023-04-23 13:08:50 +0200

    separate custom drawer

---
+++ src/layouts/CustomDrawer.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { View,StyleSheet, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useFonts } from "expo-font";
+import { DrawerContentScrollView } from "@react-navigation/drawer";
+import COLORS from "../constants/colors";
+
+const CustomDrawerContent = (props) => {
+  const [fontLoaded] = useFonts({
+    Trebuchet: require("../assets/fonts/trebuc.ttf"),
+  });
+  if (!fontLoaded) return null;
+  return (
+    <DrawerContentScrollView {...props}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Ionicons name="notifications" size={32} style={styles.title} />
+        <Text style={styles.title}> Notifications</Text>
+      </View>
+    </DrawerContentScrollView>
+  );
+};
+export default CustomDrawerContent;
+
+const styles = StyleSheet.create({
+  title: {
+    color: COLORS.white,
+    fontWeight: 400,
+    fontSize: 32,
+    fontFamily: "Trebuchet",
+    marginBottom:"2%"
+  },
+});
