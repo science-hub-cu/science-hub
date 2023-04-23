77c27915f22a52f97e35319403ea64bac89ff314
omar <202027471@std.sci.cu.edu.eg>
2023-04-23 13:44:51 +0200

    update button componant

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,25 +1,25 @@
 import React from "react";
 import { TouchableOpacity, Text } from "react-native";
 import COLORS from "../constants/colors";
-const Button = ({ title, onPress = () => {}, width = "88%", marginTop = 20, fontSize= 16,backgroundColor=COLORS.blue,fontColor=COLORS.white }) => {
+const Button = ({ title, onPress = () => {},height="12%", width = "88%", marginTop = 20, fontSize= 16,backgroundColor=COLORS.blue,fontColor=COLORS.white,borderColor=COLORS.white,borderWidth=0 }) => {
   return (
     <TouchableOpacity
-      activeOpacity={0.7}
+      activeOpacity={0.2}
       onPress={onPress}
       style={{
         marginTop: marginTop,
-        height: 50,
+        height: height,
         width: width,
         backgroundColor: backgroundColor,
         justifyContent: "center",
-        borderRadius: 25,
-        marginHorizontal: 10,
+        borderRadius: 20,
+        borderColor: borderColor,
+        borderWidth: borderWidth,
       }}
     >
       <Text
         style={{
           color: fontColor,
-          fontWeight: "bold",
           fontSize: fontSize,
           textAlign: "center",
         }}
