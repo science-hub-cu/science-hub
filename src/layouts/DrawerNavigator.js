1acb66407a9ebe8aae6e832230c0c2bd271e0b5f
omar <202027471@std.sci.cu.edu.eg>
2023-04-23 13:10:47 +0200

    clean drawer navigator file

--- src/layouts/DrawerNavigator.js
+++ src/layouts/DrawerNavigator.js
@@ -1,16 +1,9 @@
 import React from "react";
-import { View, SafeAreaView, StyleSheet, Text } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import Animated from "react-native-reanimated";
-import { useFonts } from "expo-font";
-
+import CustomDrawerContent from "./CustomDrawer";
 import {
   createDrawerNavigator,
-  DrawerContentScrollView,
 } from "@react-navigation/drawer";
 import COLORS from "../constants/colors";
-import HomeScreen from "../screens/Home";
-import VerifyScreen from "../screens/profileScreens/verifyScreen";
 export const Drawer = createDrawerNavigator();
 export const CustomizedDrawer = ({ children }) => {
   return (
@@ -30,36 +23,3 @@ export const CustomizedDrawer = ({ children }) => {
     </Drawer.Navigator>
   );
 };
-const CustomDrawerContent = (props) => {
-  const [fontLoaded] = useFonts({
-    Trebuchet: require("../assets/fonts/trebuc.ttf"),
-  });
-  if (!fontLoaded) return null;
-  return (
-    <DrawerContentScrollView {...props}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Ionicons
-          name="notifications"
-          size={32}
-          style={{ color: COLORS.white, marginTop: "2%" }}
-        />
-        <Text style={styles.title}> Notifications</Text>
-      </View>
-    </DrawerContentScrollView>
-  );
-};
-const styles = StyleSheet.create({
-  title: {
-    color: COLORS.white,
-    fontWeight: 400,
-    fontSize: 32,
-    fontFamily: "Trebuchet",
-  },
-});
-// export default CustomizedDrawer;
