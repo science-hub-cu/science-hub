843522334a4178aa75634d6e18648baf994684f3
Mahmoud Atef <mahmoudatef.coder@gmail.com>
2023-04-25 04:49:58 +0200

    add routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,5 +1,8 @@
 const ROUTES = {
   HOME_ROUTE: "Home Screen",
+  MATERIAL_ROUTE: "Material Screen",
+  ADD_ROUTE: "Add Screen",
+  SEARCH_ROUTE: "search Screen",
   AUTH_ROUTE: "Auth Screen",
   VERIFY_ROUTE: "Verify Screen",
   PROFILE_ROUTE: "Profile Screen",
