b5460e41cccbde7f50b461d27e021651295fbc93
omar <202027471@std.sci.cu.edu.eg>
2023-05-01 16:57:37 +0300

    separate content as sub commponant

---
+++ src/components/Post/Content.js
@@ -0,0 +1,37 @@
+import React, { useState, useRef } from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import COLORS from "../../constants/colors";
+
+const Content = ({ content = "", imageSource = "" }) => {
+  const [imageExist, setIsImageExist] = useState(imageSource !== "");
+  return (
+    <View style={styles.postContaner}>
+      <Text style={styles.content}>{content}</Text>
+      {imageExist && (
+        <Image
+          style={styles.image}
+          source={{
+            uri: imageSource,
+          }}
+        ></Image>
+      )}
+    </View>
+  );
+};
+
+export default Content;
+
+const styles = StyleSheet.create({
+  image: {
+    marginTop: 9,
+    width: "100%",
+    height: 300,
+    resizeMode: "cover",
+    overflow: "hidden",
+  },
+  content: {
+    color: COLORS.white,
+    paddingHorizontal: "3%",
+  },
+
+});
