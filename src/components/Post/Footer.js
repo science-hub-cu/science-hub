2ff1082b0f43382bbb06b6e192e8c40ed495c7dd
omar <202027471@std.sci.cu.edu.eg>
2023-05-01 16:37:22 +0300

    separate footer as sub commponant

---
+++ src/components/Post/Footer.js
@@ -0,0 +1,54 @@
+import React, { useState, useRef } from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import COLORS from "../../constants/colors";
+import {
+  UpVoteIcon,
+  DownVoteIcon,
+  CommentIcon,
+  SavePostIcon,
+} from "../IconLibrary";
+
+const Footer=()=>{
+return (
+  <View style={styles.footer}>
+    <View style={styles.iconContainer}>
+      <UpVoteIcon />
+      <Text style={styles.number}>{10}</Text>
+      <DownVoteIcon />
+    </View>
+    <View style={styles.iconContainer}>
+      <CommentIcon />
+      <Text style={styles.text}> Comment</Text>
+    </View>
+    <View style={styles.iconContainer}>
+      <SavePostIcon />
+      <Text style={styles.text}> Save Post</Text>
+    </View>
+  </View>
+);
+};
+export default Footer;
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 5,
+    justifyContent: "space-between",
+  },
+  number: {
+    paddingHorizontal: "5%",
+    color: "grey",
+    textAlign: "center",
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 5,
+    paddingHorizontal: "3%",
+  },
+  text: {
+    color: COLORS.gray2,
+  },
+});
