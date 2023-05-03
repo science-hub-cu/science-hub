3b25df48a01a294540e2751460509634878d420c
omar <202027471@std.sci.cu.edu.eg>
2023-05-03 18:34:04 +0300

    add use state and touch to star

--- src/components/Material/VerticalFooter.js
+++ src/components/Material/VerticalFooter.js
@@ -1,10 +1,16 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 import AntDesign from "react-native-vector-icons/AntDesign";
 import COLORS from "../../constants/colors";
 import { MaterialIcons } from "@expo/vector-icons";
 
 const VirticalFooter = () => {
+  const [isStarred, setIsStarred] = useState(false);
+
+  const handleStarPress = () => {
+    setIsStarred(!isStarred);
+  };
+
   return (
     <View
       style={{
@@ -13,12 +19,18 @@ const VirticalFooter = () => {
         alignItems: "center",
       }}
     >
-      <AntDesign name="star" size={19} color="gold" style={{}} />
-      {/* <AntDesign name="staro" size={19} color="white" style={{}} /> */}
+      <TouchableOpacity onPress={handleStarPress}>
+        {isStarred ? (
+          <AntDesign name="star" size={19} color="gold"/>
+        ) : (
+          <AntDesign name="staro" size={19} color="white" />
+        )}
+      </TouchableOpacity>
       <MaterialIcons name="keyboard-arrow-up" size={27} color="blue" />
       <Text style={{ color: COLORS.white, fontSize: 9 }}>69</Text>
       <MaterialIcons name="keyboard-arrow-down" size={27} color="white" />
     </View>
   );
 };
+
 export default VirticalFooter;
