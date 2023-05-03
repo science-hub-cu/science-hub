00bb520ca5d733c83ef4cf4d5fb2b21a5e821f4c
omar <202027471@std.sci.cu.edu.eg>
2023-05-03 16:29:28 +0300

    add all text componant & it is style

--- src/screens/material/MaterialHome.js
+++ src/screens/material/MaterialHome.js
@@ -13,8 +13,50 @@ const MaterialHome = () => {
           marginTop: "1.5%",
           marginHorizontal: "2%",
         }}
-      ></View>
-      
+      >
+          <Text
+            style={{
+              fontFamily: "TrebuchetMS",
+              paddingLeft: "5%",
+              paddingTop: "2%",
+              color: COLORS.white,
+              fontWeight: 700,
+              fontSize: 12,
+            }}
+          >
+            Subject code
+          </Text>
+          <Text
+            style={{
+              paddingLeft: "5%",
+              color: COLORS.gray1,
+              fontWeight: 400,
+              fontSize: 9,
+            }}
+          >
+            subject name
+          </Text>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <Text
+              style={{
+                paddingLeft: "5%",
+                color: COLORS.white,
+                fontWeight: 400,
+                fontSize: 10,
+                width: "90%",
+                paddingTop: "1%",
+              }}
+            >
+              Description (this subject is offf a777 so much more than what you
+              think, i have to mention that bakar is hotter more than Mo.Salah
+              with his black skin and small 7amoksha, today we will talk about
+              small 7amokshat & i wanna mention that small...
+            </Text>
+          </View>
+        </View>
+
     </SafeAreaView>
   );
 };
