6fe2b8303c438a8e841ac373fc178150cea52ffa
hngara <midomada45@gmail.com>
2023-05-07 12:03:14 +0300

    Delete account page

--- src/screens/profile/DeleteAcc.js
+++ src/screens/profile/DeleteAcc.js
@@ -1,9 +1,51 @@
-import { Text, View } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import { Text, View, StyleSheet, SafeAreaView, ScrollView } from "react-native";
+import LoadingButton from "../../components/LoadingButton";
+import COLORS from "../../constants/colors";
 const DeleteAcc = ({ navigation }) => {
+  const btn = useRef(null);
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      btn.current.setLoading(false);
+      // btn.current.setDisable(true);
+    });
+    return unsubscribe;
+  }, [navigation]);
   return (
-    <View style={{ backgroundColor: "#33363F" }}>
-      <Text>delete account</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={{ backgroundColor: "#33363F" }}>
+          <Text style={{ color: COLORS.red, fontSize: 20 }}>
+            by deleting account its is impossiple to restore your account please
+            reconsider . if you faced any problem or smth you can always use
+            help center
+          </Text>
+          <LoadingButton
+            ref={btn}
+            width="90%"
+            height={50}
+            fontSize={20}
+            title={"delete account"}
+            onPress={() => DEL()}
+            disabled={true}
+          ></LoadingButton>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#33363F",
+  },
+  container: {
+    flexGrow: 1,
+    padding: 20,
+    justifyContent: "center",
+    alignContent: "center",
+  },
+});
+
 export default DeleteAcc;
