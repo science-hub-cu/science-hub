8deedcdfe7b0ff2268b96a2312e92755a5b14e6b
omar <202027471@std.sci.cu.edu.eg>
2023-05-09 04:08:33 +0300

    no comment page

--- src/screens/NoCommentScreen.js
+++ src/screens/NoCommentScreen.js
@@ -1,38 +1,35 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
-import COLORS from "../../src/constants/colors"
-const NoCommentScreen=()=>{
-    return (
-      <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
-        <Image
-          style={styles.image}
-          source={require("../assets/images/noComments.png")}
-        ></Image>
-        <Text style={{ color: COLORS.gray2, fontWeight: 400, fontSize: 24 }}>
-          There is no Comments
-        </Text>
-        <Text style={{ color: COLORS.gray2, fontSize: 12, fontWeight: 400 }}>
-          Would you like to be the first one comment
-        </Text>
-      </View>
-    );
-}
+import { View, Text, Image, StyleSheet, TextInput } from "react-native";
+import COLORS from "../../src/constants/colors";
+const NoCommentScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={require("../assets/images/noComments.png")}
+      ></Image>
+      <Text style={styles.text1}>There is no Comments</Text>
+      <Text style={styles.text2}>
+        Would you like to be the first one comment
+      </Text>
+    </View>
+  );
+};
 export default NoCommentScreen;
 
 const styles = StyleSheet.create({
   container: {
+    justifyContent: "center",
+    alignItems: "center",
     flex: 1,
-    backgroundColor: COLORS.mainBackground,
   },
   image: {
     marginTop: 9,
   },
-  text: {
-    color: COLORS.white,
-    fontWeight: 700,
-    fontSize: 32,
-    textAlign: "center",
-    flex: 1,
-    textAlignVertical: "center",
+  text1: { color: COLORS.gray2, fontWeight: 400, fontSize: 24 },
+  text2: {
+    color: COLORS.gray2,
+    fontSize: 12,
+    fontWeight: 400,
   },
 });
