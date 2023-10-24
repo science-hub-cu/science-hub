d13fca88923a9bcd7dbd66361763e9a945869614
hazemmuuhammed <hazemmuuhammed@gmail.com>
2023-10-24 11:59:07 +0300

    removed an unnecessary object

--- src/screens/auth/LoginScreen.js
+++ src/screens/auth/LoginScreen.js
@@ -143,12 +143,5 @@ const styles = StyleSheet.create({
     paddingTop: "2%",
     color: COLORS.blue,
   },
-  overlay: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: "transparent",
-  },
+  
 });
