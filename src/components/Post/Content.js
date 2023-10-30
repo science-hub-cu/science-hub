cb9ea34c13ff249562db95cd2d75d0688b65b52b
aboelyazeed <.com>
2023-10-30 21:04:30 +0200

    popup image view

--- src/components/Post/Content.js
+++ src/components/Post/Content.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from "react";
 import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import Lightbox from "react-native-lightbox-v2";
 import COLORS from "../../constants/colors";
 
 const Content = ({ content = "", imageSource = "" }) => {
@@ -8,12 +9,14 @@ const Content = ({ content = "", imageSource = "" }) => {
     <View style={styles.postContaner}>
       <Text style={styles.content}>{content}</Text>
       {imageExist && (
+        <Lightbox underlayColor="black">
         <Image
           style={styles.image}
           source={{
             uri: imageSource,
           }}
         ></Image>
+        </Lightbox>
       )}
     </View>
   );
@@ -23,12 +26,12 @@ export default Content;
 
 const styles = StyleSheet.create({
   image: {
-    marginTop: 9,
     width: "100%",
     height: undefined,
     aspectRatio: 1,
   },
   content: {
+    marginBottom: 9,
     color: COLORS.white,
     paddingHorizontal: "3%",
   },
