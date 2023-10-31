34a4d511722eb0bd7db242beab7a0d5951f071ee
aboelyazeed <.com>
2023-10-31 18:25:58 +0200

    image zoom like instgram

--- src/components/Post/Content.js
+++ src/components/Post/Content.js
@@ -1,21 +1,54 @@
 import React, { useState, useRef } from "react";
-import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Animated,
+  StyleSheet,
+  Image,
+} from "react-native";
 import Lightbox from "react-native-lightbox-v2";
+import {
+  PinchGestureHandler,
+  State,
+  PanGestureHandler,
+} from "react-native-gesture-handler";
+import Pinchable from "react-native-pinchable";
 import COLORS from "../../constants/colors";
+import { event } from "react-native-reanimated";
 
 const Content = ({ content = "", imageSource = "" }) => {
   const [imageExist, setIsImageExist] = useState(imageSource !== "");
+  const scale = React.useRef(new Animated.Value(1)).current;
+
+  const onZoomEvent = Animated.event([{ nativeEvent: { scale } }], {
+    useNativeDriver: true,
+  });
+  const onZoomStatChange = (event) => {
+    if (event.nativeEvent.oldState == State.ACTIVE) {
+      Animated.spring(scale, {
+        toValue: 1,
+        useNativeDriver: true,
+      }).start();
+    }
+  };
+
   return (
     <View style={styles.postContaner}>
       <Text style={styles.content}>{content}</Text>
       {imageExist && (
         <Lightbox underlayColor="black">
-        <Image
-          style={styles.image}
-          source={{
-            uri: imageSource,
-          }}
-        ></Image>
+          <PinchGestureHandler
+            onGestureEvent={onZoomEvent}
+            onHandlerStateChange={onZoomStatChange}
+          >
+            <Animated.Image
+              style={[styles.image, { transform: [{ scale }] }]}
+              source={{
+                uri: imageSource,
+              }}
+            />
+          </PinchGestureHandler>
         </Lightbox>
       )}
     </View>
