4c81e89b518285a4fa9ba76078b6be3c22769d88
omar <202027471@std.sci.cu.edu.eg>
2024-01-25 06:10:33 +0200

    update AvatarBottomSheetScreen

--- src/screens/profile/avatarBottmSheetScreen.js
+++ src/screens/profile/avatarBottmSheetScreen.js
@@ -1,12 +1,36 @@
 import React from "react";
-import { View } from "react-native";
-import BottomSheet from "@gorhom/bottom-sheet";
+import { View, Text, StyleSheet } from "react-native";
+import { BottomSheetModal } from "@gorhom/bottom-sheet";
+import { useMemo } from "react";
 
-    const BottomSheetScreen = ({ navigation }) => {
+
+const AvatarBottomSheetScreen = ({ bottomSheetModalRef}) => {
+    const snapPoints = useMemo(() => ["40%", "70%"], []);
+
   return (
-    <BottomSheet snapPoints={snapPoints}>
-      <View>this is awesome</View>
-    </BottomSheet>
+    <BottomSheetModal
+      ref={bottomSheetModalRef}
+      index={0}
+      snapPoints={snapPoints}
+      style={styles.BottomSheet}
+      backgroundStyle={styles.backGround}
+    >
+      <View>
+        <Text>omar</Text>
+      </View>
+    </BottomSheetModal>
   );
-}
-export default BottomSheetScreen;
+};
+
+export default AvatarBottomSheetScreen;
+
+const styles = StyleSheet.create({
+  BottomSheet: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  backGround: {
+    borderRadius: 30,
+  },
+});
